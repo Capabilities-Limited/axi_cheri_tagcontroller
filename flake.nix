90e17913fa58bf35aa0a6cd15b6cd15253037019
Alexandre Joannou <alexandre@capabilitieslimited.co.uk>
2026-08-27 14:35:49 +0100

    Added initial github ci setup

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "CHERI tag controller";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cowsay
+            lolcat
+            gtest
+            bender
+            verilator
+          ];
+
+          GREETING = "cheri tag controller devshell";
+          RISCV = "NOT_IN_USE";
+          shellHook = ''
+            echo "$GREETING" | cowsay | lolcat
+          '';
+        };
+      };
+    };
+}
