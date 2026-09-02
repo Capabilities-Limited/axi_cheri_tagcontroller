d35ac8962a16113f11d434a1c33136c929acfca9
Alexandre Joannou <alexandre@capabilitieslimited.co.uk>
2026-09-02 11:47:34 +0100

    split local variable definition and assignment in read table lookup

--- src/tag_lookup_engine/tag_lookup_engine_table_lookups_read.sv
+++ src/tag_lookup_engine/tag_lookup_engine_table_lookups_read.sv
@@ -32,7 +32,8 @@ module tag_lookup_engine_table_lookups_read #(
 );
 
   function automatic tag_req_t desc_with_addr(tag_req_t desc, axi_addr_t addr);
-    tag_req_t ret = desc;
+    automatic tag_req_t ret;
+    ret = desc;
     ret.a_x_addr = addr;
     return ret;
   endfunction
